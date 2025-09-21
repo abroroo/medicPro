b8971c85156f3ad4fadadd0a47e2caf32c04ae96
abroroo <abroriskandarov99@gmail.com>
2025-09-21 16:33:44 +0900

    fix: added proper cookie config to allow auth requests

--- server/auth.ts
+++ server/auth.ts
@@ -35,6 +35,12 @@ export function setupAuth(app: Express) {
     resave: false,
     saveUninitialized: false,
     store: storage.sessionStore,
+    cookie: {
+      secure: ENV.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      sameSite: ENV.NODE_ENV === 'production' ? 'none' : 'lax',
+    },
   };
 
   app.set("trust proxy", 1);
